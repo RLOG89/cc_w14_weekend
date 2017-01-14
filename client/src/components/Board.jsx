b7af2303283e05a9df555d948e7e69f503c5b2a9
RLOG89 <ross.loggie@gmail.com>
2017-01-14 13:27:42 +0000

    added board.jsx

---
+++ client/src/components/Board.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Character from './Character.jsx'
+
+const Board = (props) => {
+  return (
+    <div>
+    <h3> Board </h3>
+    <Character />
+    </div>
+  )
+}
+
+export default Board
