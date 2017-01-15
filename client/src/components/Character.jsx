73583f691af6c65da8eed923c6a5f339ee747506
RLOG89 <ross.loggie@gmail.com>
2017-01-15 17:18:38 +0000

    added images as background with name on top

--- client/src/components/Character.jsx
+++ client/src/components/Character.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
 
 const Character = function({name, gender, eyeColour, hairColour, image}) {
+  const divStyle = {backgroundImage: "url('" + image + "')"}
   return (
-    <div className='character'>
-      <h5> Character </h5>
-      <img src = {image} />
-     <h4> {name} {gender} {eyeColour} {hairColour} </h4>
+    <div className='character' style={divStyle}>
+      <h5> {name} </h5>
     </div>
   )
 }
+
+// class Character extends React.Component {
+//   constructor(props) {
+//     super(props)
+//   }
+
+//   render() {
+//     const divStyle = {backgroundImage: {image}}
+//       return (
+//         <div className='character' style={divStyle}>
+//           <h5> {name} </h5>
+//         </div>
+//         )
+//   }
+// }
 
 export default Character
